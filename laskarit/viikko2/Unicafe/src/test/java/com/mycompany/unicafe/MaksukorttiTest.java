0255adf74b795193cb92b344d2cb1f0c786158ad
akuivan <akuivan@users.noreply.github.com>
2020-11-03 15:11:48 +0200

    Viikon 2 laskaritehtävien 2.kohta tehty

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -5,16 +5,45 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class MaksukorttiTest {
-
+    
     Maksukortti kortti;
-
+    
     @Before
     public void setUp() {
         kortti = new Maksukortti(10);
     }
-
+    
     @Test
     public void luotuKorttiOlemassa() {
-        assertTrue(kortti!=null);      
+        assertTrue(kortti != null);
+    }
+    
+    @Test
+    public void konstruktoriAsettaaSaldonAlussaOikein() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void rahanLatausKortilleKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(25);
+        assertEquals("saldo: 0.35", kortti.toString());
+    }
+
+// saldo vähenee oikein, jos rahaa on tarpeeksi
+    public void rahanNostaminenKortiltaVähentääSaldoaOikein() {
+        kortti.otaRahaa(5);
+        assertEquals("saldo: 0.5", kortti.toString());
+    }
+// saldo ei muutu, jos rahaa ei ole tarpeeksi
+
+    public void rahanNostoEiVieKortinSaldoaNegatiiviseksi() {
+        kortti.otaRahaa(50);
+        assertEquals("saldo: 0.1", kortti.toString());
+    }
+
+// metodi palauttaa true, jos rahat riittivät ja muuten false    
+    @Test
+    public void rahatRiitti() {
+        assertTrue(kortti.otaRahaa(5));
     }
 }
