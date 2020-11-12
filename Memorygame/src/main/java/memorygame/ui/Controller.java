898b907c1ce2ad908327c60dc4a75f1ac7760ad1
akuivan <akuivan@users.noreply.github.com>
2020-11-12 13:32:48 +0200

    Create main menu and update Controller class

--- Memorygame/src/main/java/memorygame/ui/Controller.java
+++ Memorygame/src/main/java/memorygame/ui/Controller.java
@@ -1,7 +1,13 @@
-
 package memorygame.ui;
 
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 
-public class Controller {
-    
+public class Controller implements EventHandler<MouseEvent> {
+
+    @Override
+    public void handle(MouseEvent arg0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
--- Memorygame/src/main/java/memorygame/ui/MemorygameUi.java
+++ Memorygame/src/main/java/memorygame/ui/MemorygameUi.java
@@ -1,16 +1,49 @@
 package memorygame.ui;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class MemorygameUi extends Application {
 
+    private static EventHandler<MouseEvent> handler;
+    static GridPane setting;
+
     public static void main(String[] args) {
         launch(args);
     }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        
+        this.handler = new Controller();
+
+        Button startGame = new Button("Aloita peli");
+        Scene menu = new Scene(startGame);
+
+        Button goToMenu = new Button("Palaa päävalikkoon");
+
+        startGame.setOnAction((event) -> {
+            this.setting = new GridPane();
+            this.setting.addColumn(6, goToMenu);
+            Scene game = new Scene(this.setting);
+            primaryStage.setScene(game);
+        });
+
+        goToMenu.setOnAction((event) -> {
+            primaryStage.setScene(menu);
+        });
+
+        primaryStage.setScene(menu);
+        primaryStage.show();
+
+    }
+
+    public static EventHandler<MouseEvent> gameController() {
+        return handler;
     }
 
 }
